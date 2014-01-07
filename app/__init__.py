4051f9de84935542f7cb30a1c3aedecba5bdfba1
Kyle Ames <ameske@dukes.jmu.edu>
2014-01-07 15:36:17 -0500

    Fixed app/__init__.py and removed code from when I intended to use flask-login.

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
-from flask.ext.login import LoginManager
 
 # Create the flask application
 app = Flask(__name__)
@@ -13,9 +12,6 @@ db.init_app(app)
 
 migrate = Migrate(app, db)
 manager = Manager(app)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
 
 manager.add_command('db', MigrateCommand)
 
